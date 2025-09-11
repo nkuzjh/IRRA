3464230fc79d4c39dce1eb61c1e652599c0c3d0d
zhh <nkuzjh@gmail.com>
2025-09-11 13:07:25 +0800

    exp ham_icfg_tta 遗漏实验 & exp3.2.1 exp3.3.3 还没跑完

--- shells/ham_icfg_tta/run.sh
+++ shells/ham_icfg_tta/run.sh
@@ -1,13 +1,13 @@
 
-bash shells/ham_icfg_tta/exp0.sh
-wait
-
-bash shells/ham_icfg_tta/exp1.sh
-wait
-
-bash shells/ham_icfg_tta/exp2.sh
-wait
-
-# bash shells/ham_icfg_tta/exp3.sh
+# bash shells/ham_icfg_tta/exp0.sh
 # wait
+
+# bash shells/ham_icfg_tta/exp1.sh
+# wait
+
+# bash shells/ham_icfg_tta/exp2.sh
+# wait
+
+bash shells/ham_icfg_tta/exp3.sh
+wait
 
