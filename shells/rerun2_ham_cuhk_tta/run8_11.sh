3ae0a68f8c71cc71669fd9c2dc8eefa9d7435528
zhh <nkuzjh@gmail.com>
2025-09-17 14:24:10 +0000

    at59: 58又挂了，代码还没commit。在59重写rerun2_ham_icfg_tta和rerun2_ham_cuhk_tta的shells，提交，转到fstcluster上训练

---
+++ shells/rerun2_ham_cuhk_tta/run8_11.sh
@@ -0,0 +1,13 @@
+
+
+# bash shells/rerun2_ham_cuhk_tta/exp8.sh
+# wait
+
+bash shells/rerun2_ham_cuhk_tta/exp9.sh
+wait
+
+bash shells/rerun2_ham_cuhk_tta/exp10.sh
+wait
+
+bash shells/rerun2_ham_cuhk_tta/exp11.sh
+wait
