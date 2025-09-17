516cfd68b38c503d9297f44c828bae917a2e7fa2
zhh <nkuzjh@gmail.com>
2025-09-17 13:30:56 +0800

    at65: gpu driver missing，running rerun2_ham_rstp_tta/exp7.sh to fst cluster

--- shells/rerun2_ham_rstp_tta/run.sh
+++ shells/rerun2_ham_rstp_tta/run.sh
@@ -1,24 +1,24 @@
 
-bash shells/rerun2_ham_rstp_tta/exp0.sh
-wait
+# bash shells/rerun2_ham_rstp_tta/exp0.sh
+# wait
 
-bash shells/rerun2_ham_rstp_tta/exp1.sh
-wait
+# bash shells/rerun2_ham_rstp_tta/exp1.sh
+# wait
 
-bash shells/rerun2_ham_rstp_tta/exp2.sh
-wait
+# bash shells/rerun2_ham_rstp_tta/exp2.sh
+# wait
 
-bash shells/rerun2_ham_rstp_tta/exp3.sh
-wait
+# bash shells/rerun2_ham_rstp_tta/exp3.sh
+# wait
 
-bash shells/rerun2_ham_rstp_tta/exp4.sh
-wait
+# bash shells/rerun2_ham_rstp_tta/exp4.sh
+# wait
 
-bash shells/rerun2_ham_rstp_tta/exp5.sh
-wait
+# bash shells/rerun2_ham_rstp_tta/exp5.sh
+# wait
 
-bash shells/rerun2_ham_rstp_tta/exp6.sh
-wait
+# bash shells/rerun2_ham_rstp_tta/exp6.sh
+# wait
 
 bash shells/rerun2_ham_rstp_tta/exp7.sh
 wait
