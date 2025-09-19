14019bddd6ad185165235492abe1701ddbfe1f14
zhh <nkuzjh@gmail.com>
2025-09-19 08:10:46 +0000

    at58: 之前58挂了，只有rerun2/ham_cuhk_tta/exp8 finish

---
+++ shells/rerun2_ham_icfg_tta/run8_11.sh
@@ -0,0 +1,12 @@
+
+bash shells/rerun2_ham_icfg_tta/exp8.sh
+wait
+
+bash shells/rerun2_ham_icfg_tta/exp9.sh
+wait
+
+bash shells/rerun2_ham_icfg_tta/exp10.sh
+wait
+
+bash shells/rerun2_ham_icfg_tta/exp11.sh
+wait
