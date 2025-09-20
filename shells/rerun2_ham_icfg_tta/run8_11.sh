2062e47f5ffb8cbfc50f012474c641b43fa7a9bc
zhh <nkuzjh@gmail.com>
2025-09-20 15:18:03 +0800

    fst_cluster超时，删除多余超参实验，继续跑rerun2/ham_icfg_tta/exp1011

--- shells/rerun2_ham_icfg_tta/run8_11.sh
+++ shells/rerun2_ham_icfg_tta/run8_11.sh
@@ -1,9 +1,9 @@
 
-bash shells/rerun2_ham_icfg_tta/exp8.sh
-wait
+# bash shells/rerun2_ham_icfg_tta/exp8.sh
+# wait
 
-bash shells/rerun2_ham_icfg_tta/exp9.sh
-wait
+# bash shells/rerun2_ham_icfg_tta/exp9.sh
+# wait
 
 bash shells/rerun2_ham_icfg_tta/exp10.sh
 wait
